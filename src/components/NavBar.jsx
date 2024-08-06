a4b6b224dd3dc072caabd3eb941079b51737a740
Aria Stroud <stroudaria@gmail.com>
2024-08-05 18:04:21 -0700

    Updates NavBar with updated link to trip planner and removes unused search icon and badge

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -34,9 +34,7 @@ const NavBar = () => {
       <ul
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li><a><Link to="/">Homepage</Link></a></li>
-        <li><a><Link to="/plantrip">Plan New Trip</Link></a></li>
-        {/* <li><a><Link to="/about">About</Link></a></li> */}
+        <li><a><Link to="/tripmanager">Plan New Trip</Link></a></li>
       </ul>
     </div>
   </div>
@@ -44,20 +42,6 @@ const NavBar = () => {
     <a className="btn btn-ghost text-xl">Plan My Trip</a>
   </div>
   <div className="navbar-end">
-    <button className="btn btn-ghost btn-circle">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-5 w-5"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor">
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-      </svg>
-    </button>
     <div className="dropdown dropdown-end">
       <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
         <div className="w-10 rounded-full">
@@ -71,7 +55,6 @@ const NavBar = () => {
         <li>
           <a className="justify-between">
             <Link to="/userprofile">Profile</Link>
-            <span className="badge">New</span>
           </a>
         </li>
         <li><a><Link to="/travelplan">Saved Trip</Link></a></li>
